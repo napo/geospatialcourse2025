0048d05359b43441181bd5352ca1005249f5cae9
Maurizio Napolitano <napo@fbk.eu>
2025-11-14 00:00:09 +0100

    Trento

--- scripts/run_graphhopper.sh
+++ scripts/run_graphhopper.sh
@@ -1,3 +1,3 @@
 export JAVA_TOOL_OPTIONS="-XX:-UseContainerSupport"
-wget -c https://repo1.maven.org/maven2/com/graphhopper/graphhopper-web/11.0/graphhopper-web-11.0.jar https://raw.githubusercontent.com/napo/geospatialcourse2025/refs/heads/main/data/config_trento.yml https://osmit-estratti.wmcloud.org/output/pbf/regioni/04_Trentino-Alto_Adige-S%C3%BCdtirol.osm.pbf
+wget -c https://repo1.maven.org/maven2/com/graphhopper/graphhopper-web/11.0/graphhopper-web-11.0.jar https://raw.githubusercontent.com/napo/geospatialcourse2025/refs/heads/main/data/config_trento.yml https://osmit-estratti.wmcloud.org/output/pbf/comuni/022205_Trento.osm.pbf
 java -jar graphhopper*.jar server config_trento.yml
